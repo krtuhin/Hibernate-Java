6b5447583ee85ff8163c60daf62ef50e2d30b257
krtuhin <tuhin.km2774@gmail.com>
2023-05-01 22:19:12 +0530

    HQL static query

---
+++ src/main/java/com/hql/HQL.java
@@ -0,0 +1,32 @@
+package com.hql;
+
+import com.mavenp.Student;
+import java.util.List;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HQL {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session s = factory.openSession();
+        //HQL
+        //syntax
+        String query = "from Student where department='IT'"; //the name Student is Class name not Database table name
+
+        Query q = s.createQuery(query); //getting query object
+
+        // Single result --> Unique syntax: q.uniqueResult();
+        //Multiple result --> List
+        List<Student> list = q.getResultList();
+
+        for (Student student : list) {
+            System.out.println(student.getName());
+        }
+
+        s.close();
+        factory.close();
+    }
+}
